c1849c7efda4fe3833b012829c42af19378e31b8
MiguelAMM42 <a93280@alunos.uminho.pt>
2023-03-06 23:20:26 +0000

    Dictionary all cleaned and base to the lex Parser

---
+++ TPC2/aula2/lex.py
@@ -0,0 +1,12 @@
+import ply.lex as lex
+
+
+literals = [':','-','+','\n']
+
+tokens = ('ID', 'ID_LING', 'VAL', 'LINHA_B')
+
+
+def t_ID(t): 
+    r'\w+(?= *:)'
+
+    
--- TPC2/tpc2_trataDic.py
+++ TPC2/tpc2_trataDic.py
@@ -2,6 +2,111 @@ import os
 import re
 import json
 
+
+def cleanRemissiveEntries(info):
+    rEntries = info.get('remissiveEntries')
+    if rEntries == '':
+        info['remissiveEntries'] = [] 
+        info['VAR'] = []
+    else:
+        split = re.split(r'Vid\.-', rEntries)
+        if len(split) > 1:
+            info['VAR'] = split[:1]
+        else:
+            info['VAR'] = []   
+        info['remissiveEntries'] = split[0]
+
+
+
+
+def getSIN(info):
+    subdomain = info.get('subdomain')
+    if subdomain != '':
+        split = re.split(r'SIN\.-', subdomain)
+        if len(split) > 1:
+            sins = re.split(r';', split[1])
+            info['SIN'] = sins
+        else:
+            info['SIN'] = []   
+        info['subdomain'] = split[0]
+    else:
+        info['subdomain'] = ""
+        info['SIN'] = []
+
+
+
+def cleanLanguages(langs, gaSIN, gaVid):
+    if "en" in langs:
+        enDict = {}
+        enDict["variables"] = []
+        enDict["synonyms"] = []
+        enDict["values"] = re.split(r';', langs["en"])
+        for i in range(len(enDict["values"])):
+            enVars = re.split(r'Vid\.-', enDict["values"][i])
+            enDict["values"][i] = enVars[0]
+            if len(enVars) > 1:
+                enDict["variables"].extend(enVars[:1])
+            enSins = re.split(r'SIN\.-', enDict["values"][i])
+            enDict["values"][i] = enSins[0]
+            if len(enSins) > 1:
+                enDict["variables"].extend(enSins[:1])
+        langs["en"] = enDict
+    if "es" in langs:
+        esDict = {}
+        esDict["variables"] = []
+        esDict["synonyms"] = []
+        esDict["values"] = re.split(r';', langs["es"])
+        for i in range(len(esDict["values"])):
+            esVars = re.split(r'Vid\.-', esDict["values"][i])
+            esDict["values"][i] = esVars[0]
+            if len(esVars) > 1:
+                esDict["variables"].extend(esVars[:1])
+            esSins = re.split(r'SIN\.-', esDict["values"][i])
+            esDict["values"][i] = esSins[0]
+            if len(esSins) > 1:
+                esDict["variables"].extend(esSins[:1])
+        langs["es"] = esDict
+    if "pt" in langs:
+        ptDict = {}
+        ptDict["variables"] = []
+        ptDict["synonyms"] = []
+        ptDict["values"] = re.split(r';', langs["pt"])
+        for i in range(len(ptDict["values"])):
+            ptVars = re.split(r'Vid\.-', ptDict["values"][i])
+            ptDict["values"][i] = ptVars[0]
+            if len(ptVars) > 1:
+                ptDict["variables"].extend(ptVars[:1])
+            ptSins = re.split(r'SIN\.-', ptDict["values"][i])
+            ptDict["values"][i] = ptSins[0]
+            if len(ptSins) > 1:
+                ptDict["variables"].extend(ptSins[:1])
+        langs["pt"] = ptDict
+    if "ga" in langs:
+        gaDict = {}
+        gaDict["variables"] = gaVid
+        gaDict["synonyms"] = gaSIN
+        gaDict["values"] = re.split(r';', langs["ga"])
+        langs["ga"] = gaDict
+    if "la" in langs:
+        laDict = {}
+        laDict["variables"] = []
+        laDict["synonyms"] = []
+        laDict["values"] = re.split(r';', langs["la"])
+        for i in range(len(laDict["values"])):
+            laVars = re.split(r'Vid\.-', laDict["values"][i])
+            laDict["values"][i] = laVars[0]
+            if len(laVars) > 1:
+                laDict["variables"].extend(laVars[:1])
+            laSins = re.split(r'SIN\.-', laDict["values"][i])
+            laDict["values"][i] = laSins[0]
+            if len(laSins) > 1:
+                laDict["variables"].extend(laSins[:1])
+        langs["la"] = laDict
+
+    
+
+
+
 with open('data/medicina.json', 'r') as f:
     data = json.load(f)
 
@@ -32,6 +137,17 @@ for entry in data["entries"]:
                 info[i][j] = re.sub(r'[\n\t]', r'', info[i][j])
         else:
             info[i] = re.sub(r'[\n\t]', r'', info[i])
+
+    
+    # clean remissive entries
+    cleanRemissiveEntries(info)
+    # clean synonyms
+    getSIN(info)
+    # clean languages and its synonyms and variables
+    cleanLanguages(info["languages"],info["SIN"],info["VAR"])
+    # delete SIN $ VAR
+    del info["SIN"]
+    del info["VAR"]
 
 
     treatedEntry[number] = info
